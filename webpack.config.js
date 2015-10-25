afc5205b50cc405e88a2469dc2b39418442e90da
Rei Lin <temisrei@gmail.com>
2015-10-25 16:31:38 +0800

    webpack plugin: uglifyplugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+var webpack = require('webpack');
+var uglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+
 module.exports = {
 	entry: {
 		bundle: './main.js',
@@ -12,7 +15,15 @@ module.exports = {
       		{ test: /\.css$/, loader: 'style-loader!css-loader' },
       		{ test: /\.jpg$/, loader: 'url-loader?name=tmp/[hash].[ext]&limit=8000' }
     	]
-  	}
+  	},
+  	plugins: [
+  	    new uglifyJsPlugin({
+  	      compress: {
+  	        warnings: false
+  	      },
+  	      mangle: false
+  	    })
+  	]
 };
 
 // module.exports = {
